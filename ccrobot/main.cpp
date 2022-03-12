ae8da233b1af87e3448504a927612cf96fb88e05
zhangran <28281539@qq.com>
2022-03-12 22:45:59 +0800

    sync

--- ccrobot/main.cpp
+++ ccrobot/main.cpp
@@ -8,3 +8,25 @@
  *
  ***********************************************************************************************************************
  */
+
+#include <Ogre.h>
+#include <OgreApplicationContextQt.h>
+
+#include <QApplication>
+
+class MyApp : public OgreBites::ApplicationContextQt
+{
+
+};
+
+int main(int argc, char* argv[])
+{
+    MyApp my_app;
+
+    my_app.initApp();
+    my_app.startTimer(40);
+
+    QApplication app(argc, argv);
+
+    return QApplication::exec();
+}
